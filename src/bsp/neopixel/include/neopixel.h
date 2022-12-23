195e44b4e1547da2e14badd055ea9f055cba7289
varofla <dhksrl0508@naver.com>
2022-12-23 16:40:03 +0900

    추가 : 픽셀별 설정 기능, 픽셀 색상 가져오는 기능 추가

--- src/bsp/neopixel/include/neopixel.h
+++ src/bsp/neopixel/include/neopixel.h
@@ -11,11 +11,18 @@ typedef struct {
   uint8_t pin_num;  // 네오픽셀이 연결된 핀 번호
   size_t pixel_cnt; // 네오픽셀 수
 
+  uint8_t color_r;
+  uint8_t color_g;
+  uint8_t color_b;
+  uint8_t color_w;
   bool is_init;
   Adafruit_NeoPixel *pixel_h;
 } bsp_neopixel_handler_t;
 
 bool bsp_neopixel_init(bsp_neopixel_handler_t *handler_p_);
 bool bsp_neopixel_set(bsp_neopixel_handler_t *handler_p_, uint8_t R_, uint8_t G_, uint8_t B_, uint8_t W_);
+bool bsp_neopixel_set_pixel(bsp_neopixel_handler_t *handler_p_, size_t pixel_num_, uint8_t R_, uint8_t G_, uint8_t B_, uint8_t W_);
+
+bool bsp_neopixel_get_color(bsp_neopixel_handler_t *handler_p_, uint8_t *R_, uint8_t *G_, uint8_t *B_, uint8_t *W_);
 
 #endif
--- src/bsp/neopixel/neopixel.cpp
+++ src/bsp/neopixel/neopixel.cpp
@@ -26,6 +26,39 @@ bool bsp_neopixel_set(bsp_neopixel_handler_t *handler_p_, uint8_t R_, uint8_t G_
     handler_p_->pixel_h->setPixelColor(i, color);
   }
   handler_p_->pixel_h->show();
+  handler_p_->color_r = R_;
+  handler_p_->color_g = G_;
+  handler_p_->color_b = B_;
+  handler_p_->color_w = W_;
+
+  return true;
+}
+
+bool bsp_neopixel_set_pixel(bsp_neopixel_handler_t *handler_p_, size_t pixel_num_, uint8_t R_, uint8_t G_, uint8_t B_, uint8_t W_) {
+  if (handler_p_->is_init == false) {
+    return false;
+  }
+
+  if (pixel_num_ > handler_p_->pixel_cnt) {
+    return false;
+  }
+
+  uint32_t color = handler_p_->pixel_h->Color(R_, G_, B_, W_);
+  handler_p_->pixel_h->setPixelColor(pixel_num_, color);
+  handler_p_->pixel_h->show();
+
+  return true;
+}
+
+bool bsp_neopixel_get_color(bsp_neopixel_handler_t *handler_p_, uint8_t *R_, uint8_t *G_, uint8_t *B_, uint8_t *W_) {
+  if (handler_p_->is_init == false) {
+    return false;
+  }
+
+  *R_ = handler_p_->color_r;
+  *G_ = handler_p_->color_g;
+  *B_ = handler_p_->color_b;
+  *W_ = handler_p_->color_w;
 
   return true;
 }
