f2403e9613c3cae7a3644855093c00637014fbb0
varofla <dhksrl0508@naver.com>
2023-02-01 19:35:34 +0900

    변경 : 에러 발생 시 네오픽셀도 깜박이도록 변경

--- src/modules/error_loop.cpp
+++ src/modules/error_loop.cpp
@@ -4,22 +4,27 @@
 
 #include <Arduino.h>
 
+#include "../bsp/bsp.h"
 #include "hw.h"
 #include "include/error_loop.h"
+
+extern bsp_neopixel_handler_t neopixel_h;
 
 void error_loop(rcl_ret_t error, const char *file_name_, const char *function_name_, int line_cnt_) {
   pinMode(HW_PIN_STATUS_LED, OUTPUT);
   char buff[200];
   sprintf(buff, "ERRORTYPE: %ld, FILENAME: %s, FUNCTION: %s, LINENO: %d", error, file_name_, function_name_, line_cnt_);
-  unsigned long reset_time = millis() + 10000;
+  unsigned long reset_time = millis() + 5000;
   while (1) {
     if (reset_time <= millis()) {
       digitalWrite(HW_PIN_RESET, LOW);
     }
     HW_SERIAL_DEBUG.println(buff);
     digitalWrite(HW_PIN_STATUS_LED, HIGH);
+    bsp_neopixel_set(&neopixel_h, 255, 0, 0, 0);
     delay(100);
     digitalWrite(HW_PIN_STATUS_LED, LOW);
+    bsp_neopixel_set(&neopixel_h, 0, 0, 0, 0);
     delay(100);
   }
 }
