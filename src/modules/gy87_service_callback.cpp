a6f927b3d8bfbdbf3ef06889e4d76b19fe9aaaa8
varofla <dhksrl0508@naver.com>
2023-02-08 12:21:55 +0900

    추가 : 캘리브레이션 시 네오픽셀 점등

--- src/modules/gy87_service_callback.cpp
+++ src/modules/gy87_service_callback.cpp
@@ -3,13 +3,20 @@
 #include <std_srvs/srv/trigger.h>
 
 #include "bsp/gy87/include/gy87.h"
+#include "bsp/neopixel/include/neopixel.h"
 #include "include/gy87_service_callback.h"
+
+extern bsp_neopixel_handler_t neopixel_h;
 
 void gy98_service_offset_calibration_callback(const void *req_p_, void *res_p_) {
   (void *)req_p_;
   // std_srvs__srv__Trigger_Request *req_p = (std_srvs__srv__Trigger_Request *)req_p_;
   std_srvs__srv__Trigger_Response *res_p = (std_srvs__srv__Trigger_Response *)res_p_;
 
+  uint8_t r, g, b, w;
+  bsp_neopixel_get_color(&neopixel_h, &r, &g, &b, &w);
+  bsp_neopixel_set(&neopixel_h, 255, 100, 0, 0);
   bool ret = bsp_gy87_set_offset_auto();
+  bsp_neopixel_set(&neopixel_h, r, g, b, w);
   res_p->success = ret;
 }
