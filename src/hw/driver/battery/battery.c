91cb558c7ed10a17a6ced3cbb91158405eb4a327
max cha <max@nizcorp.kr>
2025-11-20 11:11:38 +0900

    chore: add protection for SMP environment

--- src/hw/driver/battery/battery.c
+++ src/hw/driver/battery/battery.c
@@ -1,14 +1,20 @@
 #include <math.h>
 
+#include <pico/mutex.h>
+
 #include <hardware/adc.h>
 #include <hardware/gpio.h>
 
 #include "battery.h"
 
+static mutex_t s_adc_mutex;
+
 #define BATTERY_MAX_VOLTAGE 4.2 * HWCONF_BATTERY_CELL_CNT
 #define BATTERY_MIN_VOLTAGE HWCONF_BATTERY_TOTAL_LOW_ALERT_VOLTAGE
 
 bool battery_init(void) {
+  mutex_init(&s_adc_mutex);
+
   adc_init();
   adc_gpio_init(HWCONF_PIN_VOLTAGE_BATTERY);
   adc_select_input(3);
@@ -16,7 +22,13 @@ bool battery_init(void) {
 }
 
 float battery_get_voltage(void) {
-  return adc_read() * (3.3 / 4095.0) * HWCONF_BATTERY_VOLTAGE_DEVIDER;
+  mutex_enter_blocking(&s_adc_mutex);
+
+  uint16_t adc_value = adc_read();
+
+  mutex_exit(&s_adc_mutex);
+
+  return adc_value * (3.3 / 4095.0) * HWCONF_BATTERY_VOLTAGE_DEVIDER;
 }
 
 float battery_get_percentage(void) {
